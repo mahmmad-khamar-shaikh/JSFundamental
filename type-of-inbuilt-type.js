11c387c09f7f62eeaeb5064bd056f3d1446d8a2f
shahid13384@hotmail.com <shahid13384@hotmail.com>
2022-10-23 19:18:08 +0530

    rename and update

---
+++ type-of-inbuilt-type.js
@@ -0,0 +1,10 @@
+(function () {
+    console.log('Type of null -->',typeof null);
+    console.log('Type of undefined -->',typeof undefined);
+    console.log('Type of Object -->',typeof Object);
+    console.log('Type of object -->', typeof object);
+    console.log('Type of Function -->',typeof Function);
+    var fn = function () {};
+    console.log('Type of fun -->',typeof fn);
+    console.log('Type of any -->',typeof any);
+}());
