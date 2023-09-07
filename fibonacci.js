55f3660885de57e5652b536f87acbf8215d781e8
shahid13384 <shahid13384@hotmail.com>
2023-09-07 23:32:37 +0530

    fibonacciudpate

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,23 @@
-function fibonacci(number, previousResult = []) {
-    if (previousResult[number] != null) {
-        return previousResult[number];
+function fibonacciWithMemoization(num, previousResult = []) {
+    if (previousResult[num] != null) {
+        return previousResult[num];
     }
     let result;
-    if (number <= 2) {
+    if (num <= 2) {
         result = 1;
     } else {
-        result = fibonacci(number - 1, previousResult) + fibonacci(number - 2, previousResult);
+        result = fibonacciWithMemoization(num - 1, previousResult) + fibonacciWithMemoization(num - 2, previousResult);
     }
-    previousResult[number] = result;
+    previousResult[num] = result;
     return result;
 }
-console.log(fibonacci(10));
+
+function fibonacci(num){
+    if(num<=2){
+        return 1;
+    }
+    return fibonacci(num-1) + fibonacci(num-2);
+}
+
+// console.log(fibonacci(41));
+console.log(fibonacciWithMemoization(1000))
