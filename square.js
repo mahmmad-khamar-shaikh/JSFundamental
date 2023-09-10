b85a4a7bd9cb40fdcdf0dcb6c39fae2d4ea237e1
shahid13384 <shahid13384@hotmail.com>
2023-09-10 17:15:15 +0530

    update for square

--- square.js
+++ square.js
@@ -1,6 +1,17 @@
 
 const cachedResult = [];
 function square(num) {
+    let sqr = 0;
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            sqr += 1;
+        }
+    }
+    return sqr;
+};
+
+
+function squareWithMemo(num) {
     if (cachedResult[num] != null) {
         return cachedResult[num];
     }
@@ -12,10 +23,41 @@ function square(num) {
     }
     cachedResult[num] = sqr;
     return sqr;
-}
+};
 
+
+let squareStartTime = Date.now(); // set starting milliseconds;
+console.log(`Start Time ${squareStartTime}`);
 console.log(square(100000));
+console.log(`End time ${Date.now()}`);
+let totalTimeElased = Date.now() - squareStartTime;
+console.log(`Total Time elapsed ${totalTimeElased}`);
+console.log('Total time elapsed in Square function without Memoizatino - First Call', Math.floor(totalTimeElased / 1000));
+
+squareStartTime = Date.now();
 console.log(square(100000));
+totalTimeElased = Date.now() - squareStartTime;
+console.log('Total time elapsed in Square function without Memoizatino - Second Call', Math.floor(totalTimeElased / 1000));
+
+squareStartTime = Date.now();
 console.log(square(100000));
-console.log(square(100000));
-console.log(square(100000));
+totalTimeElased = Date.now() - squareStartTime;
+console.log('Total time elapsed in Square function without Memoizatino - Third Call', Math.floor(totalTimeElased / 1000));
+
+// /*************** Memoized call **************/
+
+let squareStartTimeWithMemo = Date.now(); // set starting milliseconds;
+
+console.log(squareWithMemo(100000));
+totalTimeElased = Date.now() - squareStartTimeWithMemo;
+console.log('Total time elapsed in Square function Memoized - First Call', Math.floor(totalTimeElased / 1000));
+
+squareStartTimeWithMemo = Date.now();
+console.log(squareWithMemo(100000));
+totalTimeElased = Date.now() - squareStartTimeWithMemo;
+console.log('Total time elapsed in Square function Memoized - Second Call', Math.floor(totalTimeElased / 1000));
+
+squareStartTimeWithMemo = Date.now();
+console.log(squareWithMemo(100000));
+totalTimeElased = Date.now() - squareStartTimeWithMemo;
+console.log('Total time elapsed in Square function Memoized - Third Call', Math.floor(totalTimeElased / 1000));
