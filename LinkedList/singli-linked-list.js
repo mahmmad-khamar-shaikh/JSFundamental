656f509ab9a2c053be336bcae6368dd55d97f6e8
shahid13384 <shahid13384@hotmail.com>
2024-05-26 21:56:00 +0530

    integreated with UI

--- LinkedList/singli-linked-list.js
+++ LinkedList/singli-linked-list.js
@@ -1,3 +1,5 @@
+
+const resultRef = document.getElementById("result");
 class Node {
   constructor(data) {
     this.data = data;
@@ -25,59 +27,31 @@ class LinkedList {
 // let node0 = new Node(3);
 let myList = new LinkedList();
 //console.log(myList);
-let node1 = new Node(5);
-//console.log(node1);
-let node2 = new Node(7);
-//console.log(node2)
+// let node1 = new Node(5);
+// //console.log(node1);
+// let node2 = new Node(7);
+// //console.log(node2)
 
-let node3 = new Node(9);
-//console.log(node3);
+// let node3 = new Node(9);
+// //console.log(node3);
 
-myList.appendNode(node1);
-//console.log(myList);
+// myList.appendNode(node1);
+// //console.log(myList);
 
-myList.appendNode(node2);
-//console.log(myList);
+// myList.appendNode(node2); 
+// //console.log(myList);
 
-myList.appendNode(node3);
-// console.log(myList);
+// myList.appendNode(node3);
+// // console.log(myList);
 
-function iterate(obj, stack) {
-  for (var property in obj) {
-    console.log(property);
-    if (obj.hasOwnProperty(property)) {
-      if (typeof obj[property] == "object") {
-        iterate(obj[property], stack + "." + property);
-      } else {
-        console.log(property + " " + obj[property]);
-      }
-    }
-  }
+function addNode() {
+  const nodeValue = document.getElementById("txtNode").value;
+  let node = new Node(nodeValue);
+  myList.appendNode(node);
+  resultRef.textContent = JSON.stringify(myList, undefined, 2);
 }
 
-//iterate(myList,'');
-
-const node = {
-  data: 1,
-  next: {
-    data: 3,
-    next: {
-      data: 5,
-      next: {
-        data :7,
-        next : {
-          data : 9,
-          next :  null
-        }
-      },
-    },
-  },
-};
+console.log(JSON.stringify(myList, null, 4));
 
 
-
-
-console.log( JSON.stringify(node,null,4));
-
-
-
+resultRef.textContent = JSON.stringify(myList, undefined, 2);
