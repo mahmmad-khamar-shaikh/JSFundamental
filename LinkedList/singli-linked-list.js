e799e6b95932078ecfa4c50819383eba758050a7
shahid13384 <shahid13384@hotmail.com>
2024-06-23 21:23:14 +0530

    remove nth element but first and last

--- LinkedList/singli-linked-list.js
+++ LinkedList/singli-linked-list.js
@@ -35,8 +35,8 @@ function traverseNode() {
   let currentNode = myList.head;
   console.log(JSON.stringify(myList, null, 4));
   while (currentNode) {
-   -- console.log(`Data ---> ${currentNode.data}`);
-   -- console.log(`Node ---> ${ JSON.stringify(currentNode.next, undefined, 2)}`);
+    // console.log(`Data ---> ${currentNode.data}`);
+    // console.log(`Node ---> ${ JSON.stringify(currentNode.next, undefined, 2)}`);
     currentNode = currentNode.next;
   }
 }
@@ -44,16 +44,28 @@ function traverseNode() {
 function removeNode() {
   const searchElement = document.getElementById("txtNode").value;
   let currentNode = myList.head;
+  let prevNode = myList.head;
   while (currentNode) {
     if (currentNode.data == searchElement) {
+      if (prevNode === currentNode) {
+        currentNode = currentNode.next;
+        prevNode= currentNode;
+      } else {
+        prevNode.next = currentNode.next;
+      }
       console.log(`Element Found ${currentNode.data} breaking out of loop.`);
+
       break;
     }
     console.log(
       `Element {${currentNode.data}} not found yet, moving to next element`
     );
+
+    prevNode = currentNode;
     currentNode = currentNode.next;
   }
+  resultRef.textContent = JSON.stringify(myList, undefined, 2);
+
 }
 
 console.log(JSON.stringify(myList, null, 4));
